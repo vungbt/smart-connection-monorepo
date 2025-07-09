e9e03814b6b02e641dd1628d86b022cad3cf3755
vungbt <vungbt1999@gmail.com>
2025-07-09 15:10:24 +0700

    fix: update table loading ui

--- libs/ui-components/src/table/table-loading.tsx
+++ libs/ui-components/src/table/table-loading.tsx
@@ -3,8 +3,8 @@ import { RenderIcon } from '../icons';
 
 export const TableLoading = (): ReactNode => {
   return (
-    <tr>
-      <td colSpan={1000} className="relative p-0">
+    <tr className="absolute top-0 left-0 right-0 bottom-0 w-full h-full flex items-center justify-center">
+      <td colSpan={1000} className="p-0">
         <div className="absolute top-0 left-0 right-0 bottom-0 w-full h-full flex items-center justify-center">
           <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[1]">
             <RenderIcon name="loading" />
